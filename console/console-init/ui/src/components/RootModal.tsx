ad0d74b53b0dfe40e172c179775fa871d1824f95
Ajay Pratap <apratap@localhost.localdomain>
2020-03-13 16:06:52 +0530

    refactor edit address space, review and configuration components

--- console/console-init/ui/src/components/RootModal.tsx
+++ console/console-init/ui/src/components/RootModal.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useStoreContext, MODAL_TYPES } from "context-state-reducer";
 import { DialogPrompt } from "components/common/DialogPrompt";
 import {
-  EditAddressSpace,
+  EditAddressSpaceContainer,
   CreateAddressSpace
 } from "modules/address-space/dialogs";
 
 const MODAL_COMPONENTS: any = {
   [MODAL_TYPES.CREATE_ADDRESS_SPACE]: CreateAddressSpace,
-  [MODAL_TYPES.EDIT_ADDRESS_SPACE]: EditAddressSpace,
+  [MODAL_TYPES.EDIT_ADDRESS_SPACE]: EditAddressSpaceContainer,
   [MODAL_TYPES.DELETE_ADDRESS_SPACE]: DialogPrompt,
   [MODAL_TYPES.DELETE_ADDRESS]: DialogPrompt,
   [MODAL_TYPES.PURGE_ADDRESS]: DialogPrompt
